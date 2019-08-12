665c11a20733957940b8cb4a8bd10b0f43b9d372
Thomas Wang <wglcoco@gmail.com>
2019-08-13 00:53:02 +0800

    refactor ItemUpdater file

--- src/main/java/gildedRose/ItemUpdater.java
+++ src/main/java/gildedRose/ItemUpdater.java
@@ -1,4 +1,30 @@
 package gildedRose;
 
+import static gildedRose.ItemValidator.itemHasNotReachedMaximumQuality;
+import static gildedRose.ItemValidator.itemHasNotReachedMinimumQuality;
+
 public class ItemUpdater {
+    private ItemUpdater() {
+    }
+
+    public static void incrementQualityByOne(Item item) {
+        if (itemHasNotReachedMaximumQuality(item)) {
+            item.quality++;
+        }
+    }
+
+    public static void decrementQualityByOne(Item item) {
+        if (itemHasNotReachedMinimumQuality(item)) {
+            item.quality--;
+        }
+    }
+
+    public static void setQualityToZero(Item item) {
+        item.quality = 0;
+    }
+
+    public static void decrementSellInByOne(Item item) {
+        item.sellIn--;
+    }
+
 }

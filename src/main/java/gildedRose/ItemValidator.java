fd5565563de1ec88f74fca560aba30acea6592b2
Thomas Wang <wglcoco@gmail.com>
2019-08-13 00:35:08 +0800

    refactor add ItemValidator file and move some const

---
+++ src/main/java/gildedRose/ItemValidator.java
@@ -0,0 +1,10 @@
+package gildedRose;
+
+public class ItemValidator {
+    public static final int MINIMUM_ITEM_QUALITY = 0;
+    public static final int MAXIMUM_ITEM_QUALITY = 50;
+
+    public ItemValidator() {
+    }
+
+}

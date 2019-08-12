0d66b8ebc436908f5e4d73cb7224cbec8a724f15
Thomas Wang <wglcoco@gmail.com>
2019-08-13 00:16:47 +0800

    refactor the method noSulfuras

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -1,8 +1,5 @@
 package gildedRose;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class GildedRose {
     Item[] items;
 //    List<Item> itemList = new ArrayList<>();
@@ -18,9 +15,7 @@ public class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!isAgedBrieAndBackstagePass(items[i])) {
-                if (items[i].quality > 0) {
-                    noSulfurasQualitySubOne(items[i]);
-                }
+                noSulfurasQualitySubOne(items[i]);
             } else {
                 if (items[i].quality < 50) {
                     addItemQuality(items[i]);
@@ -36,9 +31,7 @@ public class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            noSulfurasQualitySubOne(items[i]);
-                        }
+                        noSulfurasQualitySubOne(items[i]);
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
@@ -50,8 +43,10 @@ public class GildedRose {
     }
 
     private void noSulfurasQualitySubOne(Item item) {
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.quality = item.quality - 1;
+        if (item.quality > 0) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.quality = item.quality - 1;
+            }
         }
     }
 

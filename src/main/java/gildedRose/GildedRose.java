29a370bd940f026855a4cf4cd5718fec614f90a1
Thomas Wang <wglcoco@gmail.com>
2019-08-13 01:19:50 +0800

    delete some redundant code

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 public class GildedRose {
     Item[] items;
-//    List<Item> itemList = new ArrayList<>();
 private static List<UpdateQualityStrategy> updateQualityStrategies = new ArrayList<>();
 
     static {
@@ -33,39 +32,4 @@ private static List<UpdateQualityStrategy> updateQualityStrategies = new ArrayLi
         }
     }
 
-
-    private void noSulfurasQualitySubOne(Item item) {
-        if (item.quality > ItemValidator.MINIMUM_ITEM_QUALITY) {
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.quality = item.quality - 1;
-            }
-        }
-    }
-
-    private void noSulfurasSellInSubOne(Item item) {
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
-    }
-
-    private void addBackstageQuality(Item item) {
-        if (item.sellIn < 11) {
-            addItemQuality(item);
-        }
-
-        if (item.sellIn < 6) {
-            addItemQuality(item);
-        }
-    }
-
-    private void addItemQuality(Item item) {
-        if (item.quality < ItemValidator.MAXIMUM_ITEM_QUALITY) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    private boolean isAgedBrieAndBackstagePass(Item item) {
-        return item.name.equals("Aged Brie")
-                || item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
 }

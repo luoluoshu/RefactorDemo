23cc1885fe8204e3b954bb5d3b66b74d9f55c82d
Thomas Wang <thomas.wang@oocl.com>
2019-08-12 17:03:40 +0800

    init gildedRose

--- src/main/java/rentalstore/Statement.java
+++
@@ -1,7 +0,0 @@
-package rentalstore;
-
-public abstract class Statement {
-    public abstract String getHeader(Customer customer);
-    public abstract String eachRental(Rental rental);
-    public abstract String getFooter(Customer customer);
-}

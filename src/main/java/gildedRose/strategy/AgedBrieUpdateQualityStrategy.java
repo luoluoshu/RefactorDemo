49aa24185fa72306c06115a89f789e42062a62f4
Thomas Wang <wglcoco@gmail.com>
2019-08-13 01:17:36 +0800

    refactor aged brie strategy file

--- src/main/java/gildedRose/strategy/AgedBrieUpdateQualityStrategy.java
+++ src/main/java/gildedRose/strategy/AgedBrieUpdateQualityStrategy.java
@@ -1,4 +1,25 @@
 package gildedRose.strategy;
 
-public class AgedBrieUpdateQualityStrategy {
+import gildedRose.Item;
+
+import static gildedRose.ItemUpdater.decrementSellInByOne;
+import static gildedRose.ItemUpdater.incrementQualityByOne;
+import static gildedRose.ItemValidator.AGED_BRIE;
+import static gildedRose.ItemValidator.itemSellDateHasPassed;
+
+public class AgedBrieUpdateQualityStrategy implements UpdateQualityStrategy {
+    @Override
+    public boolean canHandle(String itemName) {
+        return AGED_BRIE.equals(itemName);
+    }
+
+    @Override
+    public void handle(Item item) {
+        incrementQualityByOne(item);
+        decrementSellInByOne(item);
+
+        if (itemSellDateHasPassed(item)) {
+            incrementQualityByOne(item);
+        }
+    }
 }
